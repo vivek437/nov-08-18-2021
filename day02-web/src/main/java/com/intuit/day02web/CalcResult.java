6b8e152934854f4a13791202f7f354d6d833cf13
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-11-09 15:14:45 +0530

    Day02

---
+++ day02-web/src/main/java/com/intuit/day02web/CalcResult.java
@@ -0,0 +1,13 @@
+package com.intuit.day02web;
+
+public class CalcResult {
+    private int resultValue;
+
+    public int getResultValue() {
+        return resultValue;
+    }
+
+    public void setResultValue(int resultValue) {
+        this.resultValue = resultValue;
+    }
+}
--- day02-web/src/main/java/com/intuit/day02web/HateoasExampleController.java
+++ day02-web/src/main/java/com/intuit/day02web/HateoasExampleController.java
@@ -11,12 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HateoasExampleController {
 
     @GetMapping("/sum/{num1}/{num2}")
-    public HttpEntity<Integer> add(@PathVariable int num1, @PathVariable int num2) {
-        return new ResponseEntity<>(num1 + num2, HttpStatus.OK);
+    public HttpEntity<CalcResult> add(@PathVariable int num1, @PathVariable int num2) {
+        CalcResult result = new CalcResult();
+        result.setResultValue(num1 + num2);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/difference/{num1}/{num2}")
-    public HttpEntity<Integer> subtract(@PathVariable int num1, @PathVariable int num2) {
-        return new ResponseEntity<>(num1 - num2, HttpStatus.OK);
+    public HttpEntity<CalcResult> subtract(@PathVariable int num1, @PathVariable int num2) {
+        CalcResult result = new CalcResult();
+        result.setResultValue(num1 - num2);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
