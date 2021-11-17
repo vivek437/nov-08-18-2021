4f4f05b907980eb81abb2f46dc6b6570df90182c
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-11-17 17:08:45 +0530

    Day06

--- springboot_kafka/kafka-consumer-app/src/main/java/com/intuit/kafkaconsumerapp/StockListener.java
+++ springboot_kafka/kafka-consumer-app/src/main/java/com/intuit/kafkaconsumerapp/StockListener.java
@@ -1,12 +1,34 @@
 package com.intuit.kafkaconsumerapp;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StockListener {
-    @KafkaListener(topics = "t-stock")
-    public void receiveStock(String symbol) {
-        System.out.println("CMP of " + symbol + " is " + (Math.random() * 1000));
+//    @KafkaListener(topics = "t-stock")
+//    public void receiveStock(String symbol) {
+//        System.out.println("CMP of " + symbol + " is " + (Math.random() * 1000));
+//    }
+
+    @KafkaListener(topics = "t-training", groupId = "training-dev", concurrency = "3")
+    public void receiveTrainingData(ConsumerRecord<String, String> record) {
+        System.out.println("Thread: " +  Thread.currentThread().getName() + " Key: " + record.key() + ", Partition no: " + record.partition() + ", data: " + record.value());
+    }
+
+
+    @KafkaListener(topics = "t-stock", groupId = "stock-broker")
+    public void receiveStockConsumer1(String symbol) {
+        System.out.println("Stock broker received " + symbol);
+    }
+
+    @KafkaListener(topics = "t-stock", groupId = "news-portal")
+    public void receiveStockConsumer2(String symbol) {
+        System.out.println("New portal received " + symbol);
+    }
+
+    @KafkaListener(topics = "t-stock", groupId = "tax-agency")
+    public void receiveStockConsumer3(String symbol) {
+        System.out.println("Tax agency received " + symbol);
     }
 }
--- springboot_kafka/kafka-producer-app/src/main/java/com/intuit/kafkapublisherapp/KafkaPublisherAppApplication.java
+++ springboot_kafka/kafka-producer-app/src/main/java/com/intuit/kafkapublisherapp/KafkaPublisherAppApplication.java
@@ -1,13 +1,34 @@
 package com.intuit.kafkapublisherapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
+@EnableScheduling
 public class KafkaPublisherAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaPublisherAppApplication.class, args);
     }
 
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Scheduled(fixedRate = 10, timeUnit = TimeUnit.SECONDS)
+    public void sendMessageToTrainingTopic() {
+        IntStream
+                .range(1, 11)
+                .forEach(num -> {
+                    int partitionNumber = (num % 3);
+                    kafkaTemplate.send("t-training", partitionNumber, num + "", "Data: " + num );
+                });
+    }
 }
