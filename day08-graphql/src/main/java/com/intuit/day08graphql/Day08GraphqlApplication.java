70441f6cd1594845e050b94f824d1118553e5992
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-11-19 16:30:17 +0530

    Day08

---
+++ day08-graphql/src/main/java/com/intuit/day08graphql/Day08GraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.day08graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day08GraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day08GraphqlApplication.class, args);
+    }
+
+}
---
+++ day08-graphql/src/test/java/com/intuit/day08graphql/Day08GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.day08graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day08GraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
