d70818bccc66d1d990c67eda64b2b129d4cf5167
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-11-19 16:52:41 +0530

    Day08

--- day08-graphql/src/main/java/com/intuit/day08graphql/BookQueryResolver.java
+++ day08-graphql/src/main/java/com/intuit/day08graphql/BookQueryResolver.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class BookQueryResolver implements GraphQLQueryResolver {
@@ -12,9 +13,18 @@ public class BookQueryResolver implements GraphQLQueryResolver {
     @Autowired
     private List<Book> booksList;
 
-    public List<Book> books() {
+    public List<Book> all() {
         //You can talk to other services and fetch data
         //You can talk to databases and fetch data
         return booksList;
+    }
+
+    public List<Book> books(boolean inStock) {
+        //You can talk to other services and fetch data
+        //You can talk to databases and fetch data
+        return booksList
+                .stream()
+                .filter(e -> e.isInStock() == inStock)
+                .collect(Collectors.toList());
     }
 }
